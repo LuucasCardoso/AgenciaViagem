1157d2283a699e10489898d259648a3bebbefdc5
Jonatam <jonatamsr@hotmail.com>
2018-08-30 20:59:04 -0300

    Consertando DataAnnotations da model Usuario

---
+++ AgenciaViagem/CriaBanco/Program.cs
@@ -0,0 +1,18 @@
+﻿using Models.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriaBanco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new Contexto();
+            db.Usuarios
+        }
+    }
+}
--- AgenciaViagem/Models/Entities/Usuario.cs
+++ AgenciaViagem/Models/Entities/Usuario.cs
@@ -20,6 +20,15 @@ namespace Models.Entities
         [Required]
         [Range(4, 12, ErrorMessage = "O usuário deve conter entre 4 e 12 caracteres!")]
         public string User { get; set; }
+        [Required]
+        [MaxLength(30, ErrorMessage ="Nome precisa ter no máximo 30 caracteres!")]
+        public string Nome { get; set; }
+        [Required]
+        [Range(11,11,ErrorMessage ="O campo CPF precisa ter 11 dígitos!")]
+        public string Cpf { get; set; }
+        [Required]
+        [Range(13, 14, ErrorMessage = "O campo Telefone precisa ter Código do País, do Estado e número!")]
+        public string Telefone { get; set; }
         public bool Administrador { get; set; }
         public bool Ativo { get; set; }
     }
