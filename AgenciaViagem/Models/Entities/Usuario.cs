cd91a7f01fabd62239ab95f2c93a2af49cff9255
Jonatam <jonatamsr@hotmail.com>
2018-08-30 20:47:10 -0300

    Removendo Column Orders

--- AgenciaViagem/Models/Entities/Usuario.cs
+++ AgenciaViagem/Models/Entities/Usuario.cs
@@ -11,20 +11,15 @@ namespace Models.Entities
     public class Usuario
     {
         [Key]
-        [Column(Order = 0)]
         public int Id { get; set; }
         [Required]
         [EmailAddress(ErrorMessage ="Email inválido!")]
-        [Column(Order = 1)]
         public string Email { get; set; }
         [Required]
-        [Column(Order = 2)]
         public string Password { get; set; }
         [Required]
         [Range(4, 12, ErrorMessage = "O usuário deve conter entre 4 e 12 caracteres!")]
-        [Column(Order = 3)]
         public string User { get; set; }
-        [Column(Order = 4)]
         public bool Administrador { get; set; }
         public bool Ativo { get; set; }
     }
