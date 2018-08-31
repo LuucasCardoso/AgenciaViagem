37357d9c6344dc0fbf4818381cbc52c76381b71c
Jonatam <jonatamsr@hotmail.com>
2018-08-30 21:42:07 -0300

    Lapidando condicional na população do banco

--- AgenciaViagem/CriaBanco/Program.cs
+++ AgenciaViagem/CriaBanco/Program.cs
@@ -30,6 +30,11 @@ namespace CriaBanco
                     Cpf = "12345678910",
                     Telefone = "5541999999999"
                 });
+                Console.WriteLine("Criada entidade Administrativa! User: admin Senha: admin");
+            }
+            else
+            {
+                Console.WriteLine("Entidade Administrativa já existe!");
             }
             lista = from u in db.Usuarios
                     where u.Nome == "Teste"
@@ -47,6 +52,11 @@ namespace CriaBanco
                     Cpf = "10123456789",
                     Telefone = "5541888888888"
                 });
+                Console.WriteLine("Criada entidade Teste! User: teste Senha: teste");
+            }
+            else
+            {
+                Console.WriteLine("Entidade Teste já existe!");
             }
             db.SaveChanges();
             Console.WriteLine("Banco Populado!!");
