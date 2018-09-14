9457d00ccdeaf114c0e7dc9f189abc53b00d350e
Jonatam <jonatamsr@hotmail.com>
2018-09-13 21:11:15 -0300

    Criado DAO QuartoDAO Criado controller QuartoController Criada view QuartoCadastro

---
+++ AgenciaViagem/Controllers/QuartoController.cs
@@ -0,0 +1,20 @@
+﻿using Models.DAL;
+using Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controllers
+{
+    public class QuartoController
+    {
+        static readonly QuartoDAO dao = new QuartoDAO();
+
+        public void CadastrarQuarto(Quarto quarto)
+        {
+            dao.Create(quarto);
+        }
+    }
+}
