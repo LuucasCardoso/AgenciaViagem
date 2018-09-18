28cd944d4bf77cbf3b3735c014fbbd49d4a59584
Jonatam <jonatamsr@hotmail.com>
2018-09-17 23:26:33 -0300

    Removendo temporariamente a propriedade "CidadeOrigem"

--- AgenciaViagem/Models/Entities/Passagem.cs
+++ AgenciaViagem/Models/Entities/Passagem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -23,12 +24,12 @@ namespace Models.Entities
         }
 
         //Propriedades Cidade
-        [ForeignKey("_CidadeOrigem")]
         public int CidadeOrigemId { get; set; }
-        [ForeignKey("_CidadeDestino")]
-        public int CidadeDestinoId { get; set; }
+        //[Required]
+        //[ForeignKey("_CidadeDestino")]
+        //public int CidadeDestinoId { get; set; }
         public virtual Cidade _CidadeOrigem { get; set; }
-        public virtual Cidade _CidadeDestino { get; set; }
+        //public virtual Cidade _CidadeDestino { get; set; }
 
         //Propriedades Empresas Aéreas
         public int EmpresaAereaId { get; set; }
