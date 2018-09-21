91a1fe323c30fd03972c5f0943d4e3aae7152984
Eduardo Vicente Soares Silva <eduardovicentesoares@gmail.com>
2018-09-20 21:18:35 -0300

    Carrinho com Controller, DAO, Model e não printa duplicado

---
+++ AgenciaViagem/Models/Entities/Carrinho.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Entities
+{
+
+    [Table("Carrinho")]
+    public class Carrinho
+    {
+        public int CarrinhoId { get; set; }
+
+        //Propriedades ItemCompra
+        public virtual ItemCompra _ItemCompra { get; set; }
+
+    }
+}
