5c51ffa5be7bd9bd291889551e7f4602c0b246e1
Jonatam <jonatamsr@hotmail.com>
2018-09-25 20:10:11 -0300

    Consertando datepicker passagem

--- AgenciaViagem/ViewWPF/Views/ListPassagens.xaml.cs
+++ AgenciaViagem/ViewWPF/Views/ListPassagens.xaml.cs
@@ -47,44 +47,55 @@ namespace ViewWPF.Views
             PassagemViewModel pvm = DataContext as PassagemViewModel;
             pvm.Preco = 1000;
             pvm.UsuarioId = 2;
-            Passagem passagem = new Passagem
-            {
-                PassagemId = pvm.PassagemId,
-                CidadeOrigem = pvm.CidadeOrigem,
-                CidadeDestino = pvm.CidadeDestino,
-                DataEmbarque = pvm.DataEmbarque,
-                DataVolta = pvm.DataVolta,
-                Preco = pvm.Preco,
-                UsuarioId = pvm.UsuarioId,
-                EmpresaAereaId = pvm.EmpresaAereaId
-            };
             try
             {
-                if (passagem.DataEmbarque == null)
+                if (dtDataEmbarque.SelectedDate == null)
                 {
                     throw new Exception("Favor preencher Data Embarque!");
                 }
-                if (passagem.DataVolta == null)
+                if (dtDataVolta.SelectedDate == null)
                 {
                     throw new Exception("Favor preencher Data Volta!");
                 }
-                if (passagem.EmpresaAereaId == 0)
+                Passagem passagem = new Passagem
                 {
-                    throw new Exception("Favor preencher Empresa Aerea!");
-                }
-                if (passagem.CidadeOrigem == null)
+                    PassagemId = pvm.PassagemId,
+                    CidadeOrigem = pvm.CidadeOrigem,
+                    CidadeDestino = pvm.CidadeDestino,
+                    DataEmbarque = (DateTime)dtDataEmbarque.SelectedDate,
+                    DataVolta = (DateTime)dtDataVolta.SelectedDate,
+                    Preco = pvm.Preco,
+                    UsuarioId = pvm.UsuarioId,
+                    EmpresaAereaId = pvm.EmpresaAereaId
+                };
+                try
                 {
-                    throw new Exception("Favor preencher Cidade Origem!");
+                    if (passagem.EmpresaAereaId == 0)
+                    {
+                        throw new Exception("Favor preencher Empresa Aerea!");
+                    }
+                    if (passagem.CidadeOrigem == null)
+                    {
+                        throw new Exception("Favor preencher Cidade Origem!");
+                    }
+                    if (passagem.CidadeDestino == null)
+                    {
+                        throw new Exception("Favor preencher Cidade Destino!");
+                    }
+                    controller.CadastrarPassagem(passagem);
+                    lblMessage.Content = "Passagem Reservada!";
+                    timer.Elapsed += LimpaLabel;
+                    timer.AutoReset = false;
+                    timer.Start();
                 }
-                if (passagem.CidadeDestino == null)
+                catch (Exception ex)
                 {
-                    throw new Exception("Favor preencher Cidade Destino!");
+                    lblMessage.Content = ex.Message;
+                    timer.Elapsed += LimpaLabel;
+                    timer.AutoReset = false;
+                    timer.Start();
                 }
-                controller.CadastrarPassagem(passagem);
-                lblMessage.Content = "Passagem Reservada!";
-                timer.Elapsed += LimpaLabel;
-                timer.AutoReset = false;
-                timer.Start();
+
             }
             catch (Exception ex)
             {
